71c37d8d38e29d238c376cf4ff4abdd5ab85c393
tnutzmann <tonynutzmann@icloud.com>
2022-11-16 22:37:04 +0100

    add BootleTube Base

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#!/usr/bin/python3
+from bottle import route, run, template
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@route('/')
+def home():
+    return template('home.tpl', name='BottleTube Home', content='Bla')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@route('/home2')
+def home():
+    return template('home2.tpl', name='BottleTube Home', content='Bla')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@route('/hello')
+def index():
+    return '<b>Hello world</b>!'
+
+
+# Start server
+run(host='localhost', port=8080)
---
+++ views/header.tpl
@@ -0,0 +1,13 @@
+% setdefault('title', 'Bottle Tube')
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <title>{{title}}</title>
+    </head>
+    <body>
+        <img src="     /   " class="img-fluid rounded" alt="BottleTube">
+        <h1>{{title}}</h1>
