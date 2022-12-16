7b52c6d79e54b0e80361a599ae3bc1b698ba9ab8
tnutzmann <tonynutzmann@icloud.com>
2022-12-16 22:12:23 +0100

    sqs anpassung

--- sqs.py
+++ sqs.py
@@ -5,7 +5,7 @@ session = boto3.session.Session(aws_access_key_id='',
                                 aws_session_token=''
                                 )
 sqs = session.client('sqs', region_name='us-east-1')
-r = sqs.send_message(
+msg = sqs.send_message(
     QueueUrl='https://sqs.us-east-1.amazonaws.com/790933937313/xmas2022',
     MessageBody=('Versuch 2 - 16.12.2022 - 10:03'),
     MessageAttributes={'MatNr':
@@ -23,4 +23,4 @@ r = sqs.send_message(
                       }
 )
 
-print(r)
+print(msg)
