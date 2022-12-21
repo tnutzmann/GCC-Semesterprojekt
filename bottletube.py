c79c86275d17c567f0fd344b2953b14fe12064f6
tnutzmann <tonynutzmann@icloud.com>
2022-12-21 21:39:26 +0100

    post and get  REST endpoint

--- bottletube.py
+++ bottletube.py
@@ -7,7 +7,7 @@ import requests
 import psycopg2
 import json
 
-from bottle import route, run, template, request, default_app
+from bottle import route,post, get, put, delete, run, template, request, response, default_app
 from boto3 import resource, session
 
 HOSTNAME = requests.get('http://169.254.169.254/latest/meta-data/public-hostname').text
@@ -84,6 +84,77 @@ def do_upload_post():
     # Return template
     return template('upload_success.tpl', name='Upload Image')
 
+# ------------------------------------------
+
+@post('/api/pictures')
+def creation_handler():
+    try:
+        try:
+            request_data = request.json()
+        except:
+            raise ValueError
+
+        if request_data is None:
+            raise ValueError
+
+        try:
+            category = request_data['category']
+            upload = request_data['file_upload']
+
+            
+            name, ext = os.path.splitext(upload.filename)
+            if ext not in ('.png', '.jpg', '.jpeg'):
+                raise TypeError
+
+            if not os.path.exists(SAVE_PATH):
+                os.makedirs(SAVE_PATH)
+            save_filename = f'{name}_{time.strftime("%Y%m%d-%H%M%S")}{ext}'
+            with open(f'{SAVE_PATH}{save_filename}', 'wb') as open_file:
+                open_file.write(upload.file.read())
+
+            if ext == '.png':
+                content_type = 'image/png'
+            else:
+                content_type = 'image/jpeg'
+
+            # Upload to S3
+            data = open(SAVE_PATH + save_filename, 'rb')
+            s3_resource.Bucket(BUCKET_NAME).put_object(Key=f'user_uploads/{save_filename}',Body=data,Metadata={'Content-Type': content_type},ACL='public-read')
+            # Write to DB
+            cursor.execute(f"INSERT INTO image_uploads (url, category) VALUES ('user_uploads/{save_filename}', '{category}');")
+            connection.commit()
+            id = connection.insert_id()
+
+
+        except (TypeError, KeyError):
+            raise ValueError
+
+    except ValueError:
+        response.status = 400
+        return
+
+    response.headers['Content-Type'] = 'application/json'
+    return json.dumps({'id':id, 'category':category, 'filename':f'user_uploads/{save_filename}'})
+
+@get('/api/pictures')
+def listing_handler():
+    pictures = []
+    cursor.execute('SELECT * FROM image_uploads ORDER BY id')
+    for record in cursor.fetchall():
+        pictures.append({'id': record[0], 'filename': record[1], 'category': record[2]})
+    return json.dumps(pictures)
+
+@put('/api/pictures/<id>')
+def update_handler(name):
+    '''Handles name updates'''
+    pass
+
+@delete('/api/pictures/<id>')
+def delete_handler(name):
+    '''Handles name deletions'''
+    pass
+
+# ------------------------------------------
 
 if True:
     sm_session = session.Session()
